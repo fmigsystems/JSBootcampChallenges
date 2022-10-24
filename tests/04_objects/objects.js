fc5641f647cdd676a71d9a84bae61e29e804a3fd
Codice <fmigsystems@gmail.com>
2022-10-24 20:10:19 +0900

    completed objects.js assignment

--- tests/04_objects/objects.js
+++ tests/04_objects/objects.js
@@ -1,20 +1,38 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
-function deleteProp(obj, prop) {
-
+function deleteProp(obj, prop) 
+{
+  delete obj[prop] 
+  
+  //console.log(obj)
+  return obj
 }
 
-function numObjectProps(obj) {
-
+function numObjectProps(obj) 
+{
+  let count = Object.keys(obj).length
+  return count
 }
 
-function shoppingCart(cart) {
+function shoppingCart(cart) 
+{
+  let total = 0
+  let sampleProp = ''
 
+  for(i in cart)
+  {
+    // console.log(Object.values(cart[i])[1])
+    total += Object.values(cart[i])[1]
+  }
+
+  return total
 }
 
-function compareObjects(obj1, obj2) {
-
+function compareObjects(obj1, obj2) 
+{
+  if(JSON.stringify(obj1) === JSON.stringify(obj2)) return true
+  else return false 
 }
 
 module.exports = {
