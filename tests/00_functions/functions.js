ab1289c2ce8888e4dd71950da783866667412350
Codice <fmigsystems@gmail.com>
2022-10-24 13:20:12 +0900

    first assignment completed

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,19 +1,44 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-
+function max(num1, num2, num3) {
+  if(num1 > num2) return num1
+  else if(num2 > num3) return num2
+  else return num3
 }
 
-function addUnknown() {
-
+// write a function named addUnknown that takes up to 5 arguments and adds them together
+function addUnknown(num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0) {
+  return num1 + num2 + num3 + num4 + num5 
 }
 
-function evenOrOdd() {
-
+// Write a function that accepts a number as an argument. return the string 'even' if it is an even nummber and 'odd' if it is an odd number
+function evenOrOdd(num) {
+  if( (num % 2) == 0)
+  {
+		return "even"
+	}
+  else return "odd"
 }
 
-function operator() {
-
+function operator(num1, num2, num3)
+{
+	if( (num1 + num2) == num3 )
+   {
+     return "plus"
+	}
+   else if( num1 - num2 == num3 )
+   {
+     return "minus"
+	}
+	else if( num1 * num2 == num3 )
+   {
+		return "multiply"
+	}
+	else if( num1 / num2 == num3 )
+	{
+		return "divide"
+	}	
+	else return null
 }
 
 module.exports = {
--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,20 +1,41 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  if(str.includes(letter)) return true 
+  else return false
+  
 }
 
 function isPalindrome(str) {
-
+  var original = str
+  var reversed = str.split('').reverse().join('')
+  if( original == reversed ) return true
+  else return false
 }
 
 function cap(str, letter) {
+    if(str.search(letter) == -1 )
+    {
+      return 'sorry not found'
+    }
+    else 
+    {
+      let num = str.indexOf(letter)
+      let firstLetter = str.charAt(num)
+      let nextLetter = str.charAt(num + 1)
 
+      let newStr = String(nextLetter.toUpperCase())
+      return newStr
+    }  
 }
 
-function firstCharacter(str1, str2) {
-
+function firstCharacter(str1, str2) 
+{
+  if(str1.toLowerCase().charAt(0) === str2.toLowerCase().charAt(0)) return true
+  else return false
 }
+
+//firstCharacter('today', 'yesterday');
 
 module.exports = {
   cap, firstCharacter, doesInclude, isPalindrome,
